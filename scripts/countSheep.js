a850ebbbc621b1a87f9e142e021a4cd821349248
tomlum <checkeredlines@gmail.com>
2017-11-30 21:17:55 -0500

    Hide pointer on hidden buttons

--- scripts/countSheep.js
+++ scripts/countSheep.js
@@ -217,12 +217,14 @@ function hide(element, speed) {
 	speed = speed ? (speed + "s") : (".5s");
 	document.getElementById(element).style.transition = "all " + speed + " ease-in-out";
 	document.getElementById(element).style.opacity = "0";
+	document.getElementById(element).style.cursor= "auto";
 }
 
 function show(element, speed) {
 	speed = speed ? (speed + "s") : (".5s");
 	document.getElementById(element).style.transition = "all " + speed + " ease-in-out";
 	document.getElementById(element).style.opacity = "1";
+	document.getElementById(element).style.cursor= "pointer";
 }
 
 
